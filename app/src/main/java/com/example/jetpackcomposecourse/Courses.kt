77ede18931465e2b394982a402eb2ee31a1585c3
Ima511 <abhaykumarjhaexam420@rediffmail.com>
2023-08-07 15:13:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposecourse/Courses.kt
@@ -0,0 +1,218 @@
+package com.example.jetpackcomposecourse
+
+import androidx.annotation.DrawableRes
+
+data class Courses(
+    val rating: Float,
+    val title: String,
+    @DrawableRes val thumbnail: Int,
+    val body: String
+)
+
+val course1 = Courses(
+    4.5f,
+    "The complete Android 13 Developer Course - Build 52 Apps",
+    R.drawable.course1,
+    """· Develop apps for the very latest version of Android 7 Nougat that also work on older Android devices running older versions of the Android operating system.
+
+· Download, install and configure the necessary (free) software.
+
+· Create your first app.
+
+· Build a range of apps demonstrating key aspects of the Android framework.
+
+· Test your apps on emulators or a real Android phone or tablet.
+
+· You’ll learn Java programming because Android app development requires knowledge of Java. Included are Java tutorial videos that will get you up to speed fast.
+
+· Ensure your apps work with current and older Android versions on phones and tablets.
+
+· Use Android studio, the newest version of Google's premier Android tool.
+
+· Learn how to use databases, web services, and even get your apps to speak!
+
+· Understand the all new Constraint layout, for "drag and drop" screen creation.
+
+· Use powerful libraries of code to play videos, download website data, manipulate images, and a whole lot more!
+
+· Learn MVVM android architecture pattern.
+
+· Build MVVM + ROOM Apps
+
+·  Build MVVM + Retrofit Apps
+
+·  Build MVVM with MySQL Databases Apps
+
+·  Build Apps using Firebase
+
+·  Build Apps using Firestore
+
+·  Build Apps using Authentication, Firebase Storage and much mooooreee..
+
+So, Why you are wasting Time? Enrol Now to get your feet wet in android coding....
+
+"""
+
+)
+
+val course2 = Courses(
+    4.4f,
+    "Mastering Android App Development with Kotlin [XML +COMPOSE]",
+    R.drawable.course2,
+    """
+        So you want to become an Android developer and start publishing your own apps?
+
+        Do you have no programming experience whatsoever? Or maybe you have worked with another programming language in the past?
+
+        You came to the right place, where you'll master android app development from zero to hero through step-by-step explanations and well-crafted curriculum to take you with no or probably few coding skills to start creating professional android apps.
+
+        You have a great idea for an app and you just need to learn how to make it happen? Or your friends know that you are tech-savvy and they constantly come to you with their great ideas and want you to build them?
+
+        In this course, you will learn everything you need to know, to build your own apps, no matter what you need them for. For your company, your friends, your own business, or simply for fun. You'll become a real Android App Developer by getting the best Android App development course on Udemy.
+
+        If you sign up now you will get all this Android app development content:
+
+        Over 50 hours of high quality & high definition 1080p video content
+
+        The complete code with step by step guides (in the code itself). So either you follow along in the video, or you get right into the action in Android Studio yourself.
+
+        Loads of exercises that make sure, that the knowledge really sticks.
+
+        The best Android 13 app development course money can buy.
+    """.trimIndent()
+)
+
+val course3 = Courses(4.6f,
+"The Complete Java Developer Course -Mastering Java from zero",
+    R.drawable.course3,
+    """All the essential Java keywords, operators, statements, and expressions needed to fully understand exactly what you’re coding and why - making programming easy to grasp and less frustrating
+
+You will learn the answers to questions like What is a Java class, What is polymorphism and inheritance and to apply them to your java apps.
+
+How to safely download and install all necessary coding tools with less time and no frustrating installations or setups
+
+Complete chapters on object-oriented programming and many aspects of the Java API (the protocols and tools for building applications) so you can code for all platforms and unlock your program’s user base (and potential sales)
+
+How to develop powerful Java applications using one of the most powerful Integrated Development Environments on the market, IntelliJ IDEA!
+
+Solving 41 coding exercises from real university exams like CNAM Paris university
+
+Build 5 complex projects from scratch to publish and sell."""
+    )
+
+val course4 = Courses(4.3f,
+"Mastering Design Patterns - Become a Professional Developer",
+R.drawable.course4,
+"""
+    ? Are you excited to learn design patterns, but they look complex and obscure.
+
+    ? Do you need real examples and step by step code implementation for these design patterns?
+
+    ? Are you looking to master the coding interview questions, excel in university exams , or even become  a professional Developer ???
+
+
+
+    ??? You came to the right place!  ???
+
+
+
+    If you want to reach the highest levels of software engineering, you simply MUST master design patterns.
+
+    Most employers are looking for senior software engineers with design pattern skills, and they’re willing to pay extra ?? to hire them.
+
+
+
+    ? Basically, design patterns is all about designing maintainable and extensible object-oriented software.
+
+    And those are the skills you're going to learn in this course, all using real-world examples! No pizzas, ducks, cars and coffees! Just real software examples.
+
+
+""".trimIndent())
+
+val course5 = Courses(4.2f,
+    "The Complete Data Structures & Algorithms Course in C & Java",
+    R.drawable.course5,
+    """
+        Want to land a job at a great tech company like Google, Microsoft, Facebook, Netflix, Amazon, or other companies but you are intimidated by the interview process and the coding questions? Do you find yourself feeling like you get "stuck" every time you get asked a coding question? This course is your answer. Using the strategies, lessons, and exercises in this course, you will learn how to land offers from all sorts of companies.
+
+        Many developers who are "self taught", feel that one of the main disadvantages they face compared to college educated graduates in computer science is the fact that they don't have knowledge about algorithms, data structures and the notorious Big-O Notation. Get on the same level as someone with computer science degree by learning the fundamental building blocks of computer science which will give you a big boost during interviews. You will also get access to our private online chat community with thousands of developers online to help you get through the course.
+    """.trimIndent()
+
+)
+
+val course6 = Courses(4.8f,
+    "The Complete Android JetPack Course - Mastering Jetpack",
+    R.drawable.course6,
+    """
+        Do you want to level up your Android programming skills and call yourself a Senior Android Developer? Then my You came to the right place!
+
+In this course, you are going to learn how to use the Android Jetpack suite. This will allow you not only to learn new features of Android but also make sure that your code is up to date, clean, uses best practices, and is easily maintainable. A must for any developer who wants to call himself a professional developer.
+
+
+
+In this course, you will build many apps:
+
+              - ViewModel App
+
+              - Live Data App
+
+              - Contacts Manager App using SQLite
+
+              - Contacts Manager App using ROOM 
+
+              - Phone Manager App - Data Binding
+
+              - Learning Management App - MVVM with Local ROOM Database
+
+              - Movie Pro App - MVVM with Retrofit to fetch Online Database
+
+              - Retrofit App
+
+              - Paging Movie App - Using Paging3 Library
+
+              - Counter App using Work Manager
+
+              - Contacts Manager App with Dependency Injection - Dagger App
+
+              - Greetings App using Navigation Framework
+    """.trimIndent()
+)
+
+val course7 = Courses(4.2f,
+    "The Complete Kotlin Course - Mastering Kotlin from Zero",
+    R.drawable.course9,
+    """
+        Are you confused from where you need to start learning coding?
+
+        Are you a Java developer wondering if you need to learn Kotlin?
+
+        Maybe you are an experienced Java developer who wants to learn Kotlin quickly, to be prepared for future project work.
+
+        Perhaps instead, you're an Android app developer who knows Java well, but you want to be able to move forward with Kotlin for new Android development work.
+
+        Because Kotlin works anywhere Java works, and can even be called from Java code (and vice-versa), this brings with it a massive opportunity for Java developers. Being able to work with both Java and Kotlin is going to give you a massive advantage over other programmers.
+
+        In addition, the Kotlin syntax is explored in detail. Over Twelve hours of videos are included in the course, making it one of the most comprehensive courses on the language.
+
+
+
+        Now is the perfect time for you to learn Kotlin, while it's relatively new. It has been around for a while, but only now is it being thrust into the limelight.
+
+        It's absolutely vital for Java developers to learn Kotlin to maximize future career opportunities. Objective-C developers who failed to transition to Swift, or were too slow doing so, were left behind. Don't make the same mistake they did!
+
+        If you are ready to greatly increase your career prospects by adding Kotlin experience to your skills and knowledge, then sign up for the course today.
+
+
+    """.trimIndent()
+
+)
+
+ val allCourses = listOf(
+    course1,
+    course2,
+    course3,
+    course4,
+    course5,
+    course6,
+    course7
+)
---
+++ app/src/main/java/com/example/jetpackcomposecourse/MainActivity.kt
@@ -0,0 +1,300 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.jetpackcomposecourse
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeCourseTheme {
+                // Transparent System Bars
+
+
+
+                // Navigation System
+
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)){
+
+                    // 1st step:- Create a NavController
+                    val navController = rememberNavController()
+
+                    // 2nd step:- Create a NavHost
+                    NavHost(navController = navController,
+                        startDestination = "home" ){
+                        // 3rd step :- Nav Graph Builder
+
+                        composable("home"){
+                            HomeScreen(onDetailsClick = {
+                                     navController.navigate("details/title=$it")
+                            }, onAboutClick = {
+                                navController.navigate("about")
+                            } )
+                        }
+
+                        composable("about"){
+                            AboutScreen {
+                                navController.popBackStack()
+                            }
+
+//                            AboutScreen (onNavigateUp = {navController.popBackStack()})
+                        }
+
+                        composable("details/title={title}",
+                            arguments = listOf(
+                                navArgument("title"){
+                                    type = NavType.StringType
+                                    nullable = true
+                                }
+                            ),
+                        ){
+                                navBackStackEntry ->
+                            val arguments = requireNotNull(navBackStackEntry.arguments)
+                            val title = arguments.getString("title")
+                            if (title != null) {
+                                DetailsScreen(title = title) {
+                                    navController.popBackStack()
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
+
+
+
+@Composable
+private fun HomeAppBar(onAboutClick: () -> Unit) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+        Text(text = "My Udemy Course", style = MaterialTheme.typography.headlineLarge)
+        Spacer(modifier = Modifier.weight(1f))
+        TextButton(onClick = onAboutClick) {
+            Text(text = "About", fontSize = 24.sp)
+        }
+    }
+}
+
+// 1) - Home Screen
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    onDetailsClick: (title: String) -> Unit,
+    onAboutClick: () -> Unit
+){
+
+    Scaffold() {padding ->
+        LazyColumn(contentPadding = padding){
+            item {
+                HomeAppBar(onAboutClick)
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(30.dp))
+            }
+
+            items(allCourses){
+                    item ->
+                CorseCard(
+                    item,
+                    onClick = {onDetailsClick(item.title)}
+                )
+            }
+        }
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CorseCard(item: Courses, onClick: () -> Unit) {
+    Card(modifier = Modifier
+        .padding(
+            horizontal = 16.dp,
+            vertical = 10.dp
+        )
+        .fillMaxWidth(),
+        onClick = onClick) {
+
+        Column() {
+            Image(painter = painterResource(id = item.thumbnail),
+                contentDescription = "Thumbnail of Course",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .aspectRatio(16f / 9f),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)) {
+                Text(text = item.title)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = item.body,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+
+        }
+
+    }
+
+}
+
+
+
+// Screen 2 :- About screen
+@Composable
+fun AboutScreen(onNavigateUp: () -> Unit){
+
+    Scaffold() {
+            paddingValues -> Column(Modifier.padding(paddingValues)) {
+        AppBar(title = "About", onNavigateUp)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Column(Modifier.padding(16.dp)) {
+            val udemy_link = LocalUriHandler.current
+            Text(text = "This app is a demonstration about the navigation in android jetpack compose")
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { udemy_link.openUri("https://www.udemy.com/course/the-complete-kotlin-course-mastering-kotlin-from-zero/")
+
+            }) {
+                Text(text = "View our udemy course")
+            }
+        }
+    }
+
+    }
+
+}
+
+@Composable
+fun AppBar(title: String, onNavigateUp: () -> Unit) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 10.dp)
+    ) {
+
+        IconButton(onClick = {onNavigateUp}) {
+            Icon(imageVector = Icons.Rounded.ArrowBack,
+                contentDescription = "Back Button" )
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = title, fontSize = 24.sp)
+
+    }
+}
+
+// Screen 3 - Details Screen
+@Composable
+fun DetailsScreen(title: String, onNavigateUp: () -> Unit ){
+
+    // Searching for the correct course
+    // matching the passed course title
+    val choosen_course = allCourses.first{it.title == title }
+
+    Scaffold() {paddingValues ->
+        Column(Modifier.padding(paddingValues)) {
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(vertical = 10.dp)) {
+                IconButton(onClick = {onNavigateUp}) {
+                    Icon(imageVector = Icons.Rounded.ArrowBack,
+                        contentDescription = "Go Back" )
+                }
+            }
+
+            Image(painter = painterResource(id = choosen_course.thumbnail),
+                contentDescription = "Selected Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(16f / 9f),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp).verticalScroll(
+                        rememberScrollState()
+                    )) {
+
+                Text(text = choosen_course.title, fontSize = 40.sp)
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(text = choosen_course.body,
+                    Modifier
+                        .fillMaxSize()
+                        ,
+                    fontSize = 20.sp
+                )
+
+            }
+
+        }
+
+    }
+
+}
